1f716deab090c66903f51245ae7475b8fd782462
Vinicius Seus <viniciusseus@gmail.com>
2021-03-26 12:03:21 -0300

    commit final

--- test/data.js
+++ test/data.js
@@ -7,8 +7,8 @@ var contatos = [
 ];
 
 MongoClient.connect('mongodb://127.0.0.1:27017/contatooh_test', 
-    function(err, db) {
-        if(err, db) throw err;
+    function(erro, db) {
+        if(erro) throw err;
 
         db.dropDatabase(function (err) {
             if(err) return console.log(err);
